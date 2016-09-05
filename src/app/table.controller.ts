83d53d2800e78803c52770d533015b1100b02b7c
Lionel Besson <lionel.besson@gmail.com>
2016-09-05 17:39:06 +0200

    Basic sorting capability (needs design)

--- src/app/table.controller.ts
+++ src/app/table.controller.ts
@@ -1,3 +1,11 @@
 export class TableController {
 
+  private sortType: string;
+  private sortReverse: boolean = false;
+  private searchText: string = '';
+
+  public sort(header: string) {
+    this.sortType = header;
+    this.sortReverse = !this.sortReverse;
+  }
 }
