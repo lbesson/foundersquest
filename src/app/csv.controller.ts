6a62e337f7b3ee2857fc5d2b04ad629090bb3da8
Lionel Besson <lionel.besson@gmail.com>
2016-09-05 15:42:17 +0200

    Possibility to specify delimiter

--- src/app/csv.controller.ts
+++ src/app/csv.controller.ts
@@ -3,16 +3,19 @@ import * as Papa from 'papaparse';
 export class CsvController {
 
   static $inject = ['$scope'];
+
+  private delimiter: string = '';
   private input: string;
   private data: Array<Array<string>>;
 
   private parseCsv(input: string) {
     const results = Papa.parse(this.input, {
-      header: true
+      header: true,
+      delimiter: this.delimiter
     });
 
     if (results.errors.length) {
-      alert('Unable to parse csv input: ' + results.errors[0].message);
+      alert('Unable to parse csv input => ' + results.errors[0].message);
       return;
     }
 
